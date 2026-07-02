daf090de3af54a5cbe7f521260a6bdb5dd500fc7
Juan Francisco Amézquita <jfall2789@gmail.com>
2026-07-01 23:20:22 -0500

    chore: inicializar proyecto Spring Boot y configurar PostgreSQL

---
+++ src/main/java/pe/edu/grupo1/siscol/SisColApplication.java
@@ -0,0 +1,16 @@
+package pe.edu.grupo1.siscol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisColApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisColApplication.class, args);
+		System.out.println("......Iniciando Siscol exitosamente........");
+
+	}
+
+
+}
---
+++ src/test/java/pe/edu/grupo1/siscol/SisColApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.grupo1.siscol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisColApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
